40383b75592db61fcc5461c620c245e154e3888c
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2024-06-25 12:01:52 +0200

    added Authenticator

--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module github.com/pkgz/service
 go 1.22
 
 require (
-	github.com/jessevdk/go-flags v1.5.0
+	github.com/go-jose/go-jose/v4 v4.0.2
+	github.com/jessevdk/go-flags v1.6.1
 	github.com/pkgz/logg v0.3.3
 	github.com/stretchr/testify v1.9.0
-	go.mongodb.org/mongo-driver v1.15.0
+	go.mongodb.org/mongo-driver v1.15.1
 )
 
 require (
@@ -20,9 +21,9 @@ require (
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ oauth.go
@@ -0,0 +1,186 @@
+package service
+
+import (
+	"context"
+	"crypto/rsa"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/go-jose/go-jose/v4"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"sync"
+	"time"
+)
+
+type Authenticator struct {
+	Host         string
+	ClientID     string
+	ClientSecret string
+
+	tk        *jwtToken
+	publicKey *rsa.PublicKey
+
+	sync.RWMutex
+}
+type jwtToken struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	ExpireIn     int    `json:"expires_in"`
+}
+
+func NewAuthenticator(ctx context.Context) (*Authenticator, error) {
+	t := &Authenticator{
+		Host:         os.Getenv("AUTH_HOST"),
+		ClientID:     os.Getenv("AUTH_CLIENT_ID"),
+		ClientSecret: os.Getenv("AUTH_CLIENT_SECRET"),
+	}
+	if t.Host == "" {
+		return nil, fmt.Errorf("AUTH_HOST not found")
+	}
+	if t.ClientID == "" {
+		return nil, fmt.Errorf("AUTH_CLIENT_ID not found")
+	}
+	if t.ClientSecret == "" {
+		return nil, fmt.Errorf("AUTH_CLIENT_SECRET not found")
+	}
+
+	publicKey, err := getPublicKey(ctx, t.Host)
+	if err != nil {
+		return nil, err
+	}
+	t.publicKey = publicKey
+
+	t.tk, err = t.token(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get token: %v", err)
+	}
+
+	ticker := time.NewTicker(time.Duration(t.tk.ExpireIn)*time.Second - time.Minute)
+	go func() {
+	loop:
+		for {
+			select {
+			case <-ticker.C:
+				tk, err := t.token(ctx)
+				if err != nil {
+					fmt.Printf("failed to get token: %v\n", err)
+
+					t.Lock()
+					t.tk = nil
+					t.Unlock()
+
+					tk, err = t.token(ctx)
+					if err != nil {
+						fmt.Printf("failed to get token: %v\n", err)
+					}
+				}
+
+				t.Lock()
+				t.tk = tk
+				t.Unlock()
+
+				ticker.Reset(time.Duration(t.tk.ExpireIn)*time.Second - time.Minute)
+			case <-ctx.Done():
+				break loop
+			}
+		}
+	}()
+
+	return t, nil
+}
+
+func (t *Authenticator) Token() string {
+	t.RLock()
+	defer t.RUnlock()
+	return t.tk.AccessToken
+}
+func (t *Authenticator) token(ctx context.Context) (*jwtToken, error) {
+	data := url.Values{}
+
+	if t.tk == nil {
+		data.Set("grant_type", "client_credentials")
+		data.Set("client_id", t.ClientID)
+		data.Set("client_secret", t.ClientSecret)
+	} else {
+		data.Set("grant_type", "refresh_token")
+		data.Set("refresh_token", t.tk.RefreshToken)
+	}
+
+	uri := fmt.Sprintf("%s/token", t.Host)
+	req, err := http.NewRequest("POST", uri, strings.NewReader(data.Encode()))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request to %s: %w", uri, err)
+	}
+
+	req.WithContext(ctx)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Accept", "application/json")
+
+	client := http.Client{
+		Timeout: time.Minute,
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to make request: %w", err)
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		return nil, fmt.Errorf("%d: %s", resp.StatusCode, string(body))
+	}
+
+	token := &jwtToken{}
+	if err := json.NewDecoder(resp.Body).Decode(&token); err != nil {
+		return nil, fmt.Errorf("failed to decode response from auth service: %w", err)
+	}
+
+	return token, nil
+}
+
+func getPublicKey(ctx context.Context, host string) (*rsa.PublicKey, error) {
+	req, _ := http.NewRequest("GET", fmt.Sprintf("%s/.well-known/jwks.json", host), nil)
+	req.WithContext(ctx)
+
+	client := http.Client{
+		Timeout: time.Second * 3,
+	}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("wrong status code: %d (%s)", resp.StatusCode, http.StatusText(resp.StatusCode))
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var jwks = jose.JSONWebKeySet{}
+	if err := json.Unmarshal(body, &jwks); err != nil {
+		return nil, err
+	}
+
+	publicJWKS := jwks.Keys
+	if len(publicJWKS) == 0 {
+		return nil, errors.New("public JWKS not found")
+	}
+
+	publicJWK := publicJWKS[0]
+	if !publicJWK.IsPublic() {
+		return nil, errors.New("JWK is not public key")
+	}
+
+	return publicJWK.Key.(*rsa.PublicKey), nil
+}
